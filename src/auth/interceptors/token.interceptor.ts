2e7ed687ff1772367940cb73c50b3f3f871b36df
linaalomari <linaomari99@gmail.com>
2024-07-23 02:23:12 +0300

    Remove comment

--- src/auth/interceptors/token.interceptor.ts
+++ src/auth/interceptors/token.interceptor.ts
@@ -1,53 +1,14 @@
-// import {
-//   CallHandler,
-//   ExecutionContext,
-//   Injectable,
-//   NestInterceptor,
-// } from '@nestjs/common';
-// import type { Response } from 'express';
-// import { Observable } from 'rxjs';
-// import { map } from 'rxjs/operators';
-// import { AuthService } from '../auth.service';
-// import { User } from '@prisma/client';
-
-// @Injectable()
-// export class TokenInterceptor implements NestInterceptor {
-//   constructor(private readonly authService: AuthService) {}
-
-//   intercept(
-//     context: ExecutionContext,
-//     next: CallHandler<User>,
-//   ): Observable<User> {
-//     return next.handle().pipe(
-//       map((user) => {
-//         const response = context.switchToHttp().getResponse<Response>();
-//         const token = this.authService.signToken(user);
-
-//         response.setHeader('Authorization', `Bearer ${token}`);
-//         response.cookie('token', token, {
-//           httpOnly: true,
-//           signed: true,
-//           sameSite: 'strict',
-//           secure: process.env.NODE_ENV === 'production',
-//         });
-
-//         return user;
-//       }),
-//     );
-//   }
-// }
-
 import {
   CallHandler,
   ExecutionContext,
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { Response } from 'express';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth.service';
-import { User } from '@prisma/client';
 
 @Injectable()
 export class TokenInterceptor implements NestInterceptor {
